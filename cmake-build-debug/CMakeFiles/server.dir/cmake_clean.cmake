e462e63e3f963b01d60384bca5fa419ba00ff26b
Sergio <sergioaires0227@gmail.com>
2025-04-28 13:58:26 +0200

    makefile update and naming conventions

---
+++ cmake-build-debug/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server.dir/link.d"
+  "CMakeFiles/server.dir/webserv/Brain/Brain.cpp.o"
+  "CMakeFiles/server.dir/webserv/Brain/Brain.cpp.o.d"
+  "CMakeFiles/server.dir/webserv/Location/Location.cpp.o"
+  "CMakeFiles/server.dir/webserv/Location/Location.cpp.o.d"
+  "CMakeFiles/server.dir/webserv/Server/HttpServer.cpp.o"
+  "CMakeFiles/server.dir/webserv/Server/HttpServer.cpp.o.d"
+  "CMakeFiles/server.dir/webserv/ServerConfig/Config.cpp.o"
+  "CMakeFiles/server.dir/webserv/ServerConfig/Config.cpp.o.d"
+  "CMakeFiles/server.dir/webserv/Socket/Socket.cpp.o"
+  "CMakeFiles/server.dir/webserv/Socket/Socket.cpp.o.d"
+  "CMakeFiles/server.dir/webserv/formatter.cpp.o"
+  "CMakeFiles/server.dir/webserv/formatter.cpp.o.d"
+  "CMakeFiles/server.dir/webserv/main_server.cpp.o"
+  "CMakeFiles/server.dir/webserv/main_server.cpp.o.d"
+  "CMakeFiles/server.dir/webserv/utils.cpp.o"
+  "CMakeFiles/server.dir/webserv/utils.cpp.o.d"
+  "server"
+  "server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/server.dir/link.d
@@ -0,0 +1,118 @@
+server: \
+  /usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/Scrt1.o \
+  /usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/crti.o \
+  /usr/lib/gcc/x86_64-linux-gnu/14/crtbeginS.o \
+  CMakeFiles/server.dir/webserv/Brain/Brain.cpp.o \
+  CMakeFiles/server.dir/webserv/Location/Location.cpp.o \
+  CMakeFiles/server.dir/webserv/Server/HttpServer.cpp.o \
+  CMakeFiles/server.dir/webserv/ServerConfig/Config.cpp.o \
+  CMakeFiles/server.dir/webserv/Socket/Socket.cpp.o \
+  CMakeFiles/server.dir/webserv/formatter.cpp.o \
+  CMakeFiles/server.dir/webserv/main_server.cpp.o \
+  CMakeFiles/server.dir/webserv/utils.cpp.o \
+  /usr/lib/gcc/x86_64-linux-gnu/14/libstdc++.so \
+  /usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libm.so \
+  /usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libm.so \
+  /usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libm.so \
+  /lib/x86_64-linux-gnu/libm.so.6 \
+  /lib/x86_64-linux-gnu/libmvec.so.1 \
+  /usr/lib/gcc/x86_64-linux-gnu/14/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/14/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/14/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-linux-gnu/14/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/14/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libc.so \
+  /usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libc.so \
+  /usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libc.so \
+  /lib/x86_64-linux-gnu/libc.so.6 \
+  /usr/lib/x86_64-linux-gnu/libc_nonshared.a \
+  /lib64/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-linux-gnu/14/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/14/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/14/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-linux-gnu/14/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/14/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/14/crtendS.o \
+  /usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/crtn.o \
+  /lib/x86_64-linux-gnu/libm.so.6 \
+  /lib64/ld-linux-x86-64.so.2
+
+/usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/Scrt1.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/crti.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/crtbeginS.o:
+
+CMakeFiles/server.dir/webserv/Brain/Brain.cpp.o:
+
+CMakeFiles/server.dir/webserv/Location/Location.cpp.o:
+
+CMakeFiles/server.dir/webserv/Server/HttpServer.cpp.o:
+
+CMakeFiles/server.dir/webserv/ServerConfig/Config.cpp.o:
+
+CMakeFiles/server.dir/webserv/Socket/Socket.cpp.o:
+
+CMakeFiles/server.dir/webserv/formatter.cpp.o:
+
+CMakeFiles/server.dir/webserv/main_server.cpp.o:
+
+CMakeFiles/server.dir/webserv/utils.cpp.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/libstdc++.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libm.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libm.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libm.so:
+
+/lib/x86_64-linux-gnu/libm.so.6:
+
+/lib/x86_64-linux-gnu/libmvec.so.1:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libc.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libc.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libc.so:
+
+/lib/x86_64-linux-gnu/libc.so.6:
+
+/usr/lib/x86_64-linux-gnu/libc_nonshared.a:
+
+/lib64/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/crtendS.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/14/../../../x86_64-linux-gnu/crtn.o:
+
+/lib/x86_64-linux-gnu/libm.so.6:
+
+/lib64/ld-linux-x86-64.so.2:
